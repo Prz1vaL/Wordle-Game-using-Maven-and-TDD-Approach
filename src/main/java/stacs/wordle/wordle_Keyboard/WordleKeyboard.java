7dd65c44551e8eb5d7a04aee4bd6a56e15e3870b
apm30 <m.ajaypradeep@icloud.com>
2023-01-30 18:48:09 +0000

    Creating a KeyboardLayout for the game.

---
+++ src/main/java/stacs/wordle/wordle_Keyboard/WordleKeyboard.java
@@ -0,0 +1,9 @@
+package stacs.wordle.wordle_Keyboard;
+
+/**
+ * This class is the keyboard layout fo the wordle game.It is a 2D array of the alphabets.
+ * @author 22031985
+ */
+public class WordleKeyboard {
+
+}
