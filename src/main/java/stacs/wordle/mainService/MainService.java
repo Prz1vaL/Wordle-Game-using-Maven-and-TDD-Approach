3c7a2ce7008d55eccb616b964093ae87fc05cbfa
apm30 <apm30>
2023-02-05 19:23:58 +0000

    integrating ScoreEngine into the MainClass.

--- src/main/java/stacs/wordle/mainService/MainService.java
+++ src/main/java/stacs/wordle/mainService/MainService.java
@@ -37,6 +37,9 @@ public class MainService {
 
     public static int attempts = 0;
 
+    private static int attemptActual;
+    public static double scoreHolder = SCORE_ENGINE.calculateScore(attemptActual);
+
     /**
      * The main method that holds the logic for the game.
      */
@@ -64,17 +67,18 @@ public class MainService {
                 drawWordle(guessWords, randomWord);
                 if (guessWords.get(attempts).equals(randomWord)) {
                     gameStatus = false;
+                    attemptActual = attempts + 1;
                     if (attempts == 0) {
-                        System.out.println("You guessed the word in " + (attempts + 1) + " attempt. You are a genius !");
-                        SCORE_ENGINE.calculateScore(attempts);
+                        System.out.println("You guessed the word in " + attemptActual + " attempt. You are a genius !");
+                        System.out.println("The probability of you guessing the word in " + attemptActual + " attempt was " + scoreHolder);
                     } else {
-                        System.out.println("You guessed the word in " + (attempts + 1) + " attempts. You are a genius !");
-                        SCORE_ENGINE.calculateScore(attempts);
+                        System.out.println("You guessed the word in " + attemptActual + " attempts. You are a genius !");
+                        System.out.println("The probability of you guessing the word in " + attemptActual + " attempts was " + scoreHolder);
                     }
                     gameStatus = false;
                 } else if (attempts == 5) {
                     System.out.println("You have exceeded the number of attempts. The word was " + randomWord.toUpperCase() + ". Better luck next time!");
-                    SCORE_ENGINE.calculateScore(attempts);
+
                     gameStatus = false;
                 }
                 attempts += 1;
