ee63546fac6f492ee7afaf1947774a116912552d
apm30 <apm30>
2023-02-06 13:14:57 +0000

    Adding (ClassLoader) .java & .class files and testing entire class call functions.

--- src/test/java/stacs/wordle/ClassLoaderTest.java
+++ src/test/java/stacs/wordle/ClassLoaderTest.java
@@ -8,7 +8,11 @@ class ClassLoaderTest {
     void main() throws ClassNotFoundException {
         getClass().getClassLoader().loadClass("stacs.wordle.MainService");
         getClass().getClassLoader().loadClass("stacs.wordle.ScoreEngine");
-        getClass().getClassLoader().loadClass("stacs.wordle.MainService");
+        getClass().getClassLoader().loadClass("stacs.wordle.WordChecker");
+        getClass().getClassLoader().loadClass("stacs.wordle.RandomEngine");
+        getClass().getClassLoader().loadClass("stacs.wordle.ColorAssigner");
+        getClass().getClassLoader().loadClass("stacs.wordle.WordleKeyboard");
+        getClass().getClassLoader().loadClass("stacs.wordle.WordleApp");
 
 
     }
